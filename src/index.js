a4f6b71b232432c5d8a34615a6f326fc9b14b467
Rodolphe <dutchr@live.fr>
2024-01-17 13:11:09 +0100

    :sparkles: Adding .env & config express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
 
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.json("WelcoMuuuuuu");
+});
+
+app.listen(port, () =>
+  console.log(`[SERVER] listening at http://localhost:${port}`)
+);
